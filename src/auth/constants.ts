d884d92cdd1f531ffce0eca052fe679195a881b4
Juan Camilo Toro <korot19@gmail.com>
2023-05-06 10:55:33 -0500

    Se elimina el archivo constants para evitar filtros de seguridad

--- src/auth/constants.ts
+++
@@ -1,8 +0,0 @@
-import { SetMetadata } from "@nestjs/common";
-
-export const jwtConstants = {
-  secret: '0c619614-fdf9-4d65-a811-73bb77e51dfc',
-  expiresIn: '1d',
-};
-export const IS_PUBLIC_KEY = 'isPublic';
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
